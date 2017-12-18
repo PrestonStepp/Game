8415adbe82f9f9f3224cbdd7a3b136b4e4dcb0d0
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-12-18 08:09:50 -0700

    added new code

---
+++ src/pokemon/model/Charizard.java
@@ -0,0 +1,18 @@
+package pokemon.model;
+
+public class Charizard
+{
+	public void derpDerpDerp()
+	{
+		System.out.println("asdadasdasda");
+	}
+	
+	public int annoy(int amount)
+	{
+		return 0;
+	}
+	public String sayDerpy()
+	{
+		return null;
+	}
+}
