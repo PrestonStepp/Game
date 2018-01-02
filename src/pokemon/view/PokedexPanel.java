36e3992658a579097d4c14edb8fdbb200f0ea043
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2018-01-02 09:20:08 -0700

    added more code

---
+++ src/pokemon/view/PokedexPanel.java
@@ -0,0 +1,36 @@
+package pokemon.view;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+
+public class PokedexPanel
+{
+	private PokemonController appController;
+	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evolvableBox;
+	private JTextField;
+	private JTextField;
+	private JTextField;
+	private JTextField;
+	private JTextField;
+	
+	private JTextArea;
+	private JTextArea;
+	
+	private JButton;
+	private JButton;
+	private JComboBox;
+	
+	private JPanel;
+	private JPanel;
+	private JPanel;
+	private JPanel;
+}
