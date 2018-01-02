340b8b1c541686bfd144883db4e9585c536c2f43
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2018-01-02 09:51:14 -0700

    added code

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,7 +1,15 @@
 package pokemon.view;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import pokemon.controller.PokemonController;
+
 
 public class PokedexPanel
 {
@@ -16,21 +24,32 @@ public class PokedexPanel
 	private JLabel iconLabel;
 	
 	private JCheckBox evolvableBox;
-	private JTextField;
-	private JTextField;
-	private JTextField;
-	private JTextField;
-	private JTextField;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
 	
-	private JTextArea;
-	private JTextArea;
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
 	
-	private JButton;
-	private JButton;
-	private JComboBox;
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokedexDropdown;
 	
-	private JPanel;
-	private JPanel;
-	private JPanel;
-	private JPanel;
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
+
+	
+	private void updatePokedexInfo(int index)
+	{
+		nameField.setText(appController.getPokedex().get(index).getName());
+		evolvableBox.setSelected(appController.getPokedex().get(index).isCanEvolve());
+		numberField.setText(appController.getPokedex().get(index).getNumber() + "");
+		attackField.setText(appController.getPokedex().get(index).getAttackPoints() + "");
+		healthField.setText(appController.getPokedex().get(index).getHealthPoints() + "");
+		modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier() + "");
+	}
 }
