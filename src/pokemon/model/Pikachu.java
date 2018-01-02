085a113a4a69f0ecc6555f05dbf557808f1ad04d
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2018-01-02 08:22:47 -0700

    added code

---
+++ src/pokemon/model/Pikachu.java
@@ -0,0 +1,26 @@
+package pokemon.model;
+
+public class Pikachu extends Pokemon implements Fire
+{
+	public Pikachu(int number, String name)
+	{
+		super(number, name);
+	}
+
+	public void derpDerpDerp()
+	{
+		System.out.println("asdadasdasda");
+	}
+	
+	public int annoy(int amount)
+	{
+		int annoyance = -913124;
+		
+		return annoyance;
+	}
+	
+	public String sayPikachu()
+	{
+		return "Pikachu";	
+	}
+}
